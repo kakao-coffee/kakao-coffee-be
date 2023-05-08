b9b5e53811bafb4f9a35edb53ef7b154df0f5f9d
rhsehfm33 <44993366+rhsehfm33@users.noreply.github.com>
2023-05-09 03:58:21 +0900

    style: Change API name and controller location to more proper one

--- src/main/java/com/kakaoCoffee/member/controller/MemberController.java
+++ src/main/java/com/kakaoCoffee/member/controller/MemberController.java
@@ -1,25 +1,23 @@
-package com.kakaoCoffee.pointHistory.controller;
+package com.kakaoCoffee.member.controller;
 
 import com.kakaoCoffee.common.dto.ApiResponse;
-import com.kakaoCoffee.common.security.UserDetailsImpl;
 import com.kakaoCoffee.pointHistory.dto.PointChargeRequestDto;
 import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@Tag(name = "포인트 충전 API")
+@Tag(name = "회원 API")
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/api/point-charges")
-public class PointChargeController {
+@RequestMapping("/api/members")
+public class MemberController {
 
-    @PostMapping("/charge")
+    @PatchMapping("/point")
     @Operation(
             summary = "포인트를 충전하는 API",
             description = "사용자 식별값, 충전금액을 받아 포인트를 충전"
@@ -28,5 +26,4 @@ public class PointChargeController {
         // TODO: Apply service and change return value null.
         return ApiResponse.successOf(HttpStatus.OK, null);
     }
-
 }
